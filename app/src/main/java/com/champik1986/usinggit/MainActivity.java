dbb6e9ffaf2096fdbf2d862a09d745cb7a2a775b
ChampiK86 <champik1986@gmail.com>
2020-12-15 13:31:13 +0200

    first

---
+++ app/src/main/java/com/champik1986/usinggit/MainActivity.java
@@ -0,0 +1,19 @@
+package com.champik1986.usinggit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String hello = "Hello Git!";
+        String hello1 = "Hello Git!";
+
+
+    }
+}
